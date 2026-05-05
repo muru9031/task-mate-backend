2c6e81192e683e3dd1086c5fa0f5d73d2308ec04
unknown <muruganandham.a2025lcse@sece.ac.in>
2026-05-05 23:18:21 +0530

    allow netlify frontend cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,23 @@ require('./config/db');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://roaring-youtiao-278de8.netlify.app'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+app.options('*', cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Task Mate Backend Running'));
