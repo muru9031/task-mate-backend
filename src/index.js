27000b625c1e7218f68116ca5e0585c2f373b5b1
unknown <muruganandham.a2025lcse@sece.ac.in>
2026-05-05 22:26:13 +0530

    add health check route

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => res.send('Task Mate Backend Running'));
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/tasks', require('./routes/tasks'));
